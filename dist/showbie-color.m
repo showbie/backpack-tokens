8999931d3a10bc0329833929141b4a5e58979f5a
Jeff <rustyangel@gmail.com>
2018-12-19 14:07:19 -0700

    Test build

---
+++ dist/showbie-color.m
@@ -0,0 +1,75 @@
+#import "UIColor+Showbie.h"
+#import "UIColor+HexColor.h"
+#import "UIDevice+BlurSupport.h"
+
+@implementation UIColor (Showbie)
+
+
++ (UIColor *)sb_blue_100 {
+    return [UIColor colorWithARGB:0xfff1faff];
+}
++ (UIColor *)sb_blue_200 {
+    return [UIColor colorWithARGB:0xffdcf3ff];
+}
++ (UIColor *)sb_blue_300 {
+    return [UIColor colorWithARGB:0xffcbeeff];
+}
++ (UIColor *)sb_blue_400 {
+    return [UIColor colorWithARGB:0xffbae8ff];
+}
++ (UIColor *)sb_blue_500 {
+    return [UIColor colorWithARGB:0xff8ad8ff];
+}
++ (UIColor *)sb_blue_600 {
+    return [UIColor colorWithARGB:0xff29b1f0];
+}
++ (UIColor *)sb_blue_700 {
+    return [UIColor colorWithARGB:0xff009fe8];
+}
++ (UIColor *)sb_blue_800 {
+    return [UIColor colorWithARGB:0xff0084c2];
+}
+
++ (UIColor *)sb_grey_100 {
+    return [UIColor colorWithARGB:0xfff8f8f8];
+}
++ (UIColor *)sb_grey_200 {
+    return [UIColor colorWithARGB:0xfff0f0f0];
+}
++ (UIColor *)sb_grey_300 {
+    return [UIColor colorWithARGB:0xffecedee];
+}
++ (UIColor *)sb_grey_400 {
+    return [UIColor colorWithARGB:0xffe9e9eb];
+}
++ (UIColor *)sb_grey_500 {
+    return [UIColor colorWithARGB:0xffcac9cd];
+}
++ (UIColor *)sb_grey_600 {
+    return [UIColor colorWithARGB:0xffb9bbbd];
+}
++ (UIColor *)sb_grey_700 {
+    return [UIColor colorWithARGB:0xff90a8b2];
+}
++ (UIColor *)sb_grey_725 {
+    return [UIColor colorWithARGB:0xff8c97a1];
+}
++ (UIColor *)sb_grey_775 {
+    return [UIColor colorWithARGB:0xff898f95];
+}
++ (UIColor *)sb_grey_800 {
+    return [UIColor colorWithARGB:0xff7a8184];
+}
++ (UIColor *)sb_grey_900 {
+    return [UIColor colorWithARGB:0xff989fa6];
+}
+
++ (UIColor *)sb_red_100 {
+    return [UIColor colorWithARGB:0xfffce5e5];
+}
++ (UIColor *)sb_red_200 {
+    return [UIColor colorWithARGB:0xfff07f7f];
+}
++ (UIColor *)sb_red_300 {
+    return [UIColor colorWithARGB:0xfffc3d39];
+}
