04ce8ad89abdbb8ac9e9e0e1f90d3f12edf30c66
Jeff <rustyangel@gmail.com>
2019-01-03 13:25:27 -0700

    Add template for Swift

---
+++ dist/showbie-color.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+extension UIColor {
+    struct Backpack {
+        static let sb_blue_100 = UIColor(rgb: 0xf1faff)
+        static let sb_blue_200 = UIColor(rgb: 0xdcf3ff)
+        static let sb_blue_300 = UIColor(rgb: 0xcbeeff)
+        static let sb_blue_400 = UIColor(rgb: 0xbae8ff)
+        static let sb_blue_500 = UIColor(rgb: 0x8ad8ff)
+        static let sb_blue_600 = UIColor(rgb: 0x29b1f0)
+        static let sb_blue_700 = UIColor(rgb: 0x009fe8)
+        static let sb_blue_800 = UIColor(rgb: 0x0084c2)
+
+        static let sb_grey_100 = UIColor(rgb: 0xf8f8f8)
+        static let sb_grey_200 = UIColor(rgb: 0xf0f0f0)
+        static let sb_grey_300 = UIColor(rgb: 0xecedee)
+        static let sb_grey_400 = UIColor(rgb: 0xe9e9eb)
+        static let sb_grey_500 = UIColor(rgb: 0xcac9cd)
+        static let sb_grey_600 = UIColor(rgb: 0xb9bbbd)
+        static let sb_grey_700 = UIColor(rgb: 0x90a8b2)
+        static let sb_grey_725 = UIColor(rgb: 0x8c97a1)
+        static let sb_grey_775 = UIColor(rgb: 0x898f95)
+        static let sb_grey_800 = UIColor(rgb: 0x7a8184)
+        static let sb_grey_900 = UIColor(rgb: 0x989fa6)
+
+        static let sb_red_100 = UIColor(rgb: 0xfce5e5)
+        static let sb_red_200 = UIColor(rgb: 0xf07f7f)
+        static let sb_red_300 = UIColor(rgb: 0xfc3d39)
+
+    }
+}
