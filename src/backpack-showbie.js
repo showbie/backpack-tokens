33ad318e8a7559221af7438c9a8d15d81f35e05b
Jeff <rustyangel@gmail.com>
2019-05-02 16:22:19 -0600

    Update color exports to objects

--- src/backpack-showbie.js
+++ src/backpack-showbie.js
@@ -1,65 +1,67 @@
-module.exports = {
-  colors: {
-    grey: [
-      '#f8f8f8', // 50
-      '#ecedee', // 100
-      '#e9e9eb', // replace with 100?
-      '', // 200
-      '', // 300
-      '#c4c4ca', // 400 blessed
-      '#b9bbbd', // 500 blessed
-      '#989fa6', // 600
-      '#7a8184', // 700
-      '#545454', // 800
-      '', // 900
-    ],
-
-    slate: [
-      '', // 50
-      '', // 200
-      '#94b2c1', // 500
-      '#90a8b2',
-      '#8aa4af', // 600 blessed
-      '', // 800
-    ],
-
-    blue: [
-      '#f1faff', // 50 blessed
-      '#e5f8ff',
-      '#dcf3ff',
-      '#cbeeff', // 100 blessed
-      '#bae8ff', // 200
-      '#8ad8ff', // 300
-      '', // 400
-      '#29b1f0', // 500 blessed
-      '#009fe8', // 600 blessed
-      '#0084c2', // 700
-      '', // 800
-      '', // 900
-    ],
-
-    green: [
-      '', // 100
-      '', // 200
-      '#5dbe6e', // 500 blessed
-      '', // 600
-      '', // 800
-    ],
-
-    yellow: [
-      '#faf0a6', // 100
-      '#f7df68', // 200
-      'rgb(221, 198, 42)', // 500
-      '', // 600
-      '', // 800
-    ],
-
-    red: [
-      '#fce5e5', // 100 blessed
-      '', // 200
-      '#f07f7f', // 500 blessed
-      '#fc3d39', // 600
-      '#e10000', // 800 blessed
-    ],
+export const colors = {
+  grey: {
+    50: '#f8f8f8',
+    100: '#ecedee',
+    '1xx': '#e9e9eb', // replace with 100?
+    200: '',
+    300: '',
+    400: '#c4c4ca', // blessed
+    500: '#b9bbbd', // blessed
+    600: '#989fa6',
+    700: '#7a8184',
+    800: '#545454',
+    900: '',
   },
+
+  slate: {
+    50: '',
+    200: '',
+    500: '#94b2c1',
+    '5xx': '#90a8b2',
+    600: '#8aa4af', // blessed
+    800: '',
+  },
+
+  blue: {
+    50: '#f1faff', // blessed
+    xx1: '#e5f8ff',
+    xx2: '#dcf3ff',
+    100: '#cbeeff', // blessed
+    200: '#bae8ff',
+    300: '#8ad8ff',
+    400: '',
+    500: '#29b1f0', // blessed
+    600: '#009fe8', // blessed
+    700: '#0084c2',
+    800: '',
+    900: '',
+  },
+
+  green: {
+    100: '',
+    200: '',
+    500: '#5dbe6e', // blessed
+    600: '',
+    800: '',
+  },
+
+  yellow: {
+    100: '#faf0a6',
+    200: '#f7df68',
+    500: 'rgb(221, 198, 42)',
+    600: '',
+    800: '',
+  },
+
+  red: {
+    100: '#fce5e5', // blessed
+    200: '',
+    500: '#f07f7f', // blessed
+    600: '#fc3d39',
+    800: '#e10000', // blessed
+  },
+};
+
+export default {
+  colors: colors,
 };
