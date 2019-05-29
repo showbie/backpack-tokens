2b0fa2c8f1da602c42a7962368e2d0ee09135671
Jeff <rustyangel@gmail.com>
2019-05-29 09:07:05 -0600

    Build output

--- dist/showbie-color.swift
+++ dist/showbie-color.swift
@@ -1,36 +1,68 @@
+////
+///   BACKPACK COLOURS
+///   v1.0.0
+////
+
 import UIKit
 
 @objc
 extension UIColor {
-    static let sbBlue100 = UIColor(red: 0.9450980392156862, green: 0.9803921568627451, blue: 1, alpha: 1)
-    static let sbBlue200 = UIColor(red: 0.8627450980392157, green: 0.9529411764705882, blue: 1, alpha: 1)
-    static let sbBlue300 = UIColor(red: 0.796078431372549, green: 0.9333333333333333, blue: 1, alpha: 1)
-    static let sbBlue400 = UIColor(red: 0.7294117647058823, green: 0.9098039215686274, blue: 1, alpha: 1)
-    static let sbBlue500 = UIColor(red: 0.5411764705882353, green: 0.8470588235294118, blue: 1, alpha: 1)
-    static let sbBlue600 = UIColor(red: 0.1607843137254902, green: 0.6941176470588235, blue: 0.9411764705882353, alpha: 1)
-    static let sbBlue700 = UIColor(red: 0, green: 0.6235294117647059, blue: 0.9098039215686274, alpha: 1)
-    static let sbBlue800 = UIColor(red: 0, green: 0.5176470588235295, blue: 0.7607843137254902, alpha: 1)
+    ///
+    /// Grey
+    ///
+    static let sbeGrey50 = UIColor(red: 0.9725490196078431, green: 0.9725490196078431, blue: 0.9725490196078431, alpha: 1)
+    static let sbeGrey100 = UIColor(red: 0.9254901960784314, green: 0.9294117647058824, blue: 0.9333333333333333, alpha: 1)
+    static let sbeGrey400 = UIColor(red: 0.7686274509803922, green: 0.7686274509803922, blue: 0.792156862745098, alpha: 1)
+    static let sbeGrey500 = UIColor(red: 0.7254901960784313, green: 0.7333333333333333, blue: 0.7411764705882353, alpha: 1)
+    static let sbeGrey600 = UIColor(red: 0.596078431372549, green: 0.6235294117647059, blue: 0.6509803921568628, alpha: 1)
+    static let sbeGrey700 = UIColor(red: 0.47843137254901963, green: 0.5058823529411764, blue: 0.5176470588235295, alpha: 1)
+    static let sbeGrey800 = UIColor(red: 0.32941176470588235, green: 0.32941176470588235, blue: 0.32941176470588235, alpha: 1)
+    static let sbeGreyXx1 = UIColor(red: 0.9137254901960784, green: 0.9137254901960784, blue: 0.9215686274509803, alpha: 1)
 
-    static let sbGrey100 = UIColor(red: 0.9725490196078431, green: 0.9725490196078431, blue: 0.9725490196078431, alpha: 1)
-    static let sbGrey200 = UIColor(red: 0.9411764705882353, green: 0.9411764705882353, blue: 0.9411764705882353, alpha: 1)
-    static let sbGrey300 = UIColor(red: 0.9254901960784314, green: 0.9294117647058824, blue: 0.9333333333333333, alpha: 1)
-    static let sbGrey400 = UIColor(red: 0.9137254901960784, green: 0.9137254901960784, blue: 0.9215686274509803, alpha: 1)
-    static let sbGrey500 = UIColor(red: 0.792156862745098, green: 0.788235294117647, blue: 0.803921568627451, alpha: 1)
-    static let sbGrey600 = UIColor(red: 0.7254901960784313, green: 0.7333333333333333, blue: 0.7411764705882353, alpha: 1)
-    static let sbGrey700 = UIColor(red: 0.5647058823529412, green: 0.6588235294117647, blue: 0.6980392156862745, alpha: 1)
-    static let sbGrey725 = UIColor(red: 0.5490196078431373, green: 0.592156862745098, blue: 0.6313725490196078, alpha: 1)
-    static let sbGrey775 = UIColor(red: 0.5372549019607843, green: 0.5607843137254902, blue: 0.5843137254901961, alpha: 1)
-    static let sbGrey800 = UIColor(red: 0.47843137254901963, green: 0.5058823529411764, blue: 0.5176470588235295, alpha: 1)
-    static let sbGrey900 = UIColor(red: 0.596078431372549, green: 0.6235294117647059, blue: 0.6509803921568628, alpha: 1)
+    ///
+    /// Slate
+    ///
+    static let sbeSlate500 = UIColor(red: 0.5803921568627451, green: 0.6980392156862745, blue: 0.7568627450980392, alpha: 1)
+    static let sbeSlate600 = UIColor(red: 0.5411764705882353, green: 0.6431372549019608, blue: 0.6862745098039216, alpha: 1)
+    static let sbeSlateXx5 = UIColor(red: 0.5647058823529412, green: 0.6588235294117647, blue: 0.6980392156862745, alpha: 1)
 
-    static let sbRed100 = UIColor(red: 0.9882352941176471, green: 0.8980392156862745, blue: 0.8980392156862745, alpha: 1)
-    static let sbRed200 = UIColor(red: 0.9411764705882353, green: 0.4980392156862745, blue: 0.4980392156862745, alpha: 1)
-    static let sbRed300 = UIColor(red: 0.9882352941176471, green: 0.23921568627450981, blue: 0.2235294117647059, alpha: 1)
+    ///
+    /// Blue
+    ///
+    static let sbeBlue50 = UIColor(red: 0.9450980392156862, green: 0.9803921568627451, blue: 1, alpha: 1)
+    static let sbeBlue100 = UIColor(red: 0.8627450980392157, green: 0.9529411764705882, blue: 1, alpha: 1)
+    static let sbeBlue200 = UIColor(red: 0.796078431372549, green: 0.9333333333333333, blue: 1, alpha: 1)
+    static let sbeBlue300 = UIColor(red: 0.7294117647058823, green: 0.9098039215686274, blue: 1, alpha: 1)
+    static let sbeBlue400 = UIColor(red: 0.5411764705882353, green: 0.8470588235294118, blue: 1, alpha: 1)
+    static let sbeBlue500 = UIColor(red: 0.1607843137254902, green: 0.6941176470588235, blue: 0.9411764705882353, alpha: 1)
+    static let sbeBlue600 = UIColor(red: 0, green: 0.6235294117647059, blue: 0.9098039215686274, alpha: 1)
+    static let sbeBlue700 = UIColor(red: 0, green: 0.5176470588235295, blue: 0.7607843137254902, alpha: 1)
 
-    static let sbGreen500 = UIColor(red: 0.36470588235294116, green: 0.7450980392156863, blue: 0.43137254901960786, alpha: 1)
+    ///
+    /// Green
+    ///
+    static let sbeGreen500 = UIColor(red: 0.36470588235294116, green: 0.7450980392156863, blue: 0.43137254901960786, alpha: 1)
 
-    static let sbBrandFeide = UIColor(red: 0.023529411764705882, green: 0.5607843137254902, blue: 0.5529411764705883, alpha: 1)
-    static let sbBrandGoogle = UIColor(red: 0.8627450980392157, green: 0.3058823529411765, blue: 0.2549019607843137, alpha: 1)
-    static let sbBrandShowbie = UIColor(red: 0, green: 0.6235294117647059, blue: 0.9098039215686274, alpha: 1)
+    ///
+    /// Yellow
+    ///
+    static let sbeYellow100 = UIColor(red: 0.9803921568627451, green: 0.9411764705882353, blue: 0.6509803921568628, alpha: 1)
+    static let sbeYellow200 = UIColor(red: 0.9686274509803922, green: 0.8745098039215686, blue: 0.40784313725490196, alpha: 1)
+    static let sbeYellow500 = UIColor(red: 0.8666666666666667, green: 0.7764705882352941, blue: 0.16470588235294117, alpha: 1)
 
+    ///
+    /// Red
+    ///
+    static let sbeRed100 = UIColor(red: 0.9882352941176471, green: 0.8980392156862745, blue: 0.8980392156862745, alpha: 1)
+    static let sbeRed500 = UIColor(red: 0.9411764705882353, green: 0.4980392156862745, blue: 0.4980392156862745, alpha: 1)
+    static let sbeRed600 = UIColor(red: 0.9882352941176471, green: 0.23921568627450981, blue: 0.2235294117647059, alpha: 1)
+    static let sbeRed800 = UIColor(red: 0.8823529411764706, green: 0, blue: 0, alpha: 1)
+
+    ///
+    /// Brand
+    ///
+    static let sbeBrandFeide = UIColor(red: 0.023529411764705882, green: 0.5607843137254902, blue: 0.5529411764705883, alpha: 1)
+    static let sbeBrandGoogle = UIColor(red: 0.8627450980392157, green: 0.3058823529411765, blue: 0.2549019607843137, alpha: 1)
+    static let sbeBrandMicrosoft = UIColor(red: 0.027450980392156862, green: 0.6509803921568628, blue: 0.9411764705882353, alpha: 1)
+    static let sbeBrandShowbie = UIColor(red: 0, green: 0.6235294117647059, blue: 0.9098039215686274, alpha: 1)
 }
---
+++ dist/socrative-color.swift
@@ -0,0 +1,37 @@
+////
+///   BACKPACK COLOURS
+///   v1.0.0
+////
+
+import UIKit
+
+@objc
+extension UIColor {
+    ///
+    /// Grey
+    ///
+    static let socGrey100 = UIColor(red: 0.9490196078431372, green: 0.9529411764705882, blue: 0.9568627450980393, alpha: 1)
+    static let socGrey200 = UIColor(red: 0.8431372549019608, green: 0.8470588235294118, blue: 0.8549019607843137, alpha: 1)
+    static let socGrey300 = UIColor(red: 0.7254901960784313, green: 0.7333333333333333, blue: 0.7411764705882353, alpha: 1)
+    static let socGrey500 = UIColor(red: 0.2980392156862745, green: 0.29411764705882354, blue: 0.2901960784313726, alpha: 1)
+
+    ///
+    /// Cyan
+    ///
+    static let socCyan100 = UIColor(red: 0.7686274509803922, green: 0.9333333333333333, blue: 0.9607843137254902, alpha: 1)
+    static let socCyan300 = UIColor(red: 0.47058823529411764, green: 0.8470588235294118, blue: 0.9176470588235294, alpha: 1)
+    static let socCyan500 = UIColor(red: 0.023529411764705882, green: 0.7176470588235294, blue: 0.8470588235294118, alpha: 1)
+    static let socCyan700 = UIColor(red: 0.01568627450980392, green: 0.5725490196078431, blue: 0.6705882352941176, alpha: 1)
+    static let socCyan900 = UIColor(red: 0.00784313725490196, green: 0.3411764705882353, blue: 0.4, alpha: 1)
+
+    ///
+    /// Green
+    ///
+    static let socGreen500 = UIColor(red: 0.36470588235294116, green: 0.7450980392156863, blue: 0.43137254901960786, alpha: 1)
+
+    ///
+    /// Orange
+    ///
+    static let socOrange100 = UIColor(red: 1, green: 0.9372549019607843, blue: 0.8509803921568627, alpha: 1)
+    static let socOrange500 = UIColor(red: 1, green: 0.3764705882352941, blue: 0.16862745098039217, alpha: 1)
+}
