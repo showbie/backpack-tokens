9a13224fb903aefab1eb1767a2cbb3d8097581c4
Jeff <rustyangel@gmail.com>
2019-09-20 10:40:20 -0600

    New: Add js export template

---
+++ dist/socrative-color.js
@@ -0,0 +1,47 @@
+/**
+ *    BACKPACK COLOURS
+ *    v0.2.4
+ */
+
+var colors = {
+  grey: {
+    50: '#f8f8f8',
+    100: '#e6e5e7',
+    200: '#cdccd0',
+    300: '#b9bbbd',
+  },
+
+  orange: {
+    50: '#fff7e5',
+    100: '#ffe9b7',
+    500: '#ffc857',
+    600: '#efa946',
+  },
+
+  green: {
+    50: '#e7f6ea',
+    100: '#c9e9cf',
+    500: '#5dbe6e',
+  },
+
+  cyan: {
+    50: '#e4fbff',
+    500: '#00b5e2',
+    600: '#0099c7',
+  },
+
+  blue: {
+    900: '#273b4c',
+  },
+
+  brand: {
+    feide: '#068f8d',
+    google: '#dc4e41',
+    microsoft: '#07a6f0',
+    showbie: '#009fe8',
+    socrative: '#ff602b',
+  },
+}
+
+exports.colors = colors;
+export { colors };
