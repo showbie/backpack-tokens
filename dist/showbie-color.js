c230c7ebb2960659a5206e84388cdcd83df85590
Jeff <rustyangel@gmail.com>
2019-09-20 12:18:15 -0600

    New: Add js export template (#81)

---
+++ dist/showbie-color.js
@@ -0,0 +1,47 @@
+/**
+ *    BACKPACK COLOURS
+ *    v0.2.4
+ */
+
+var colors = {
+  grey: {
+    400: '#c4c4ca',
+    500: '#b9bbbd',
+  },
+
+  slate: {
+    600: '#8aa4af',
+  },
+
+  blue: {
+    50: '#f1faff',
+    100: '#dcf3ff',
+    200: '#cbeeff',
+    500: '#29b1f0',
+    600: '#009fe8',
+    700: '#0084c2',
+  },
+
+  green: {
+    500: '#5dbe6e',
+  },
+
+  yellow: {
+  },
+
+  red: {
+    100: '#fce5e5',
+    500: '#f07f7f',
+    800: '#e10000',
+  },
+
+  brand: {
+    feide: '#068f8d',
+    google: '#dc4e41',
+    microsoft: '#07a6f0',
+    showbie: '#009fe8',
+    socrative: '#ff602b',
+  },
+}
+
+exports.colors = colors;
